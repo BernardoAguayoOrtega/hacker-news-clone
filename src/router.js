ab5516d660799270ceb67a1b9ab49a52dfdde1a8
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-27 09:38:26 -0600

    Create dynamic routes for all the pages

--- src/router.js
+++ src/router.js
@@ -8,12 +8,18 @@ class RouterHandler {
 	}
 
 	createRoutes() {
-		const routes = [{ path: '/', page: Stories }];
+		const routes = [
+			{ path: '/', page: Stories },
+			{ path: '/new', page: Stories },
+      { path: '/ask', page: Stories },
+			{ path: '/show', page: Stories },
+			{ path: '/favorites', page: Stories }
+		];
 
 		routes.forEach((route) => {
 			router
 				.on(route.path, () => {
-					console.log(route.page())
+					route.page()
 				})
 				.resolve();
 		});
