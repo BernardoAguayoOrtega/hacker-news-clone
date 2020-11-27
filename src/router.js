9773160a465c6eda1f1eca0e444e8f12b5dd2e0a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-27 09:22:48 -0600

    Fix minor bugs with navigo

--- src/router.js
+++ src/router.js
@@ -1,5 +1,23 @@
+import Stories from './pages/stories.js';
+
 const router = new Navigo(null, true, '#');
 
 class RouterHandler {
-  constructor(){}
+	constructor() {
+		this.createRoutes();
+	}
+
+	createRoutes() {
+		const routes = [{ path: '/', page: Stories }];
+
+		routes.forEach((route) => {
+			router
+				.on(route.path, () => {
+					console.log(route.page())
+				})
+				.resolve();
+		});
+	}
 }
+
+export default RouterHandler;
