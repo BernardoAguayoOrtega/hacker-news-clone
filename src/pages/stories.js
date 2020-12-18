1207a2c5dbc65b5a6c64c70704994e758c039014
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-12-18 17:52:57 -0600

    see data with styles

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -1,28 +1,34 @@
 import view from '../utils/view.js';
-import Story from '../components/Story.js'
+import Story from '../components/Story.js';
 
 export default async function Stories(path) {
-   const stories = await getStories(path);
-   const hasStories = stories.length > 0;
-                     
-   view.innerHTML = `<div>
-     ${hasStories ? stories.map(story => Story(story)).join('') : 'No stories'}
-   </div>`;  
+	const stories = await getStories(path);
+	const hasStories = stories.length > 0;
+
+	view.innerHTML = `<div>
+     ${
+				hasStories
+					? stories
+							.map((story, index) => Story({ ...story, index: index + 1 }))
+							.join('')
+					: 'No stories'
+			}
+   </div>`;
 }
 
 async function getStories(path) {
 	const isHomeRoute = path === '/';
-   const isNewRoute = path === '/new';
-   
+	const isNewRoute = path === '/new';
+
 	if (isHomeRoute) {
 		path = '/news';
 	} else if (isNewRoute) {
 		path = '/newest';
-   }
-   
-   const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
-   
-   const stories = await response.json();
-   
+	}
+
+	const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+
+	const stories = await response.json();
+
 	return stories;
 }
